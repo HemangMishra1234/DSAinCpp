e4d0cc7ca120e60d3e699bc3d5353b6ab4bb2f72
Hemang <hemangmishranew@gmail.com>
2023-11-14 09:45:45 +0530

    Day 1

---
+++ Q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int T, n,k;
+    std::cin >> T;
+    while (T-- > 0)
+    {   
+        std:: string str;
+        std:: cin >> n;
+        std:: cin >> k;
+        std:: cin >> str;
+        
+    }
+    
+}
